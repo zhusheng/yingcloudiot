1ff55dfb070b77eaf151e0b42a6bcdfe8a60cd2f
Jusen <1537017271@qq.com>
2019-09-06 14:58:33 +0800

    update

--- yingcloudiot/settings.py
+++ yingcloudiot/settings.py
@@ -99,12 +99,8 @@ WSGI_APPLICATION = 'yingcloudiot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': '12345678',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
